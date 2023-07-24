fcac9afb8692c0b8c1b1f2168241bef98b411b4a
Tamira2804 <tamira2804@gmail.com>
2023-07-24 11:27:16 +0200

    user profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
+import Profile from './Profile';
+import Statistics from './Statistics';
+import user from '../data/user.json';
+import data from '../data/data.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    // <div
+    //   style={{
+    //     height: '100vh',
+    //     display: 'flex',
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //     fontSize: 40,
+    //     color: '#010101',
+    //   }}
+    // >
+    <>
+      <Profile
+        avatar={user.avatar}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+    </>
+    // </div>
   );
 };
