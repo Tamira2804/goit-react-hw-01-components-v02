f78ab5ef622b6adf1e8175f4bfaa83be28269e82
Tamira2804 <tamira2804@gmail.com>
2023-07-27 20:30:13 +0200

    profile correction stats list

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -12,22 +12,22 @@ function Profile(props) {
         <p className={css.name}>{username}</p>
         <p className={css.tag}>@{tag}</p>
         <p className={css.location}>{location}</p>
-
-        <ul className={css.stats}>
-          <li className={css.item}>
-            <span className={css.label}>Followers</span>
-            <span className={css.quantity}>{followers}</span>
-          </li>
-          <li className={css.item}>
-            <span className={css.label}>Views</span>
-            <span className={css.quantity}>{views}</span>
-          </li>
-          <li className={css.item}>
-            <span className={css.label}>Likes</span>
-            <span className={css.quantity}>{likes}</span>
-          </li>
-        </ul>
       </div>
+
+      <ul className={css.stats}>
+        <li className={css.item}>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{followers}</span>
+        </li>
+        <li className={css.item}>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{views}</span>
+        </li>
+        <li className={css.item}>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{likes}</span>
+        </li>
+      </ul>
     </div>
   );
 }
--- src/components/Profile/Profile.styled.jsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-export const UserProfile = styled.div`
-  display: block,
-  width: 100,
-  marginLeft: auto,
-  marginRight: auto,
-  backgroudColor: #111,
-`;
